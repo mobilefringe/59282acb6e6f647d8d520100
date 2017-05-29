73c37bff55d7e27fc29ab3572dcff48a2a8a7ddf
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-29 14:45:29 -0400

    The Gateway 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -14,3 +14,38 @@ function show_content(){
 function render_instagram(data){
     $('#instafeed').html(data)
 }
+
+function store_search() {
+    $('#close_search_results').click(function(){
+        $(this).hide();
+        $('#store_search_img').show();
+        $('#store_search_results').html('');
+        $('#store_search_results').hide();
+        $('#store_search').val('')
+    });
+    $('#store_search').keyup(function(){
+        if ($('#store_search').val() == ""){
+            $('#store_search_results').html('');
+            $('#store_search_results').hide();
+            $('#close_search_results').hide();
+        } else {
+            $('#store_search_img').hide();
+            $('#close_search_results').show();
+            $('#store_search_results').html('');
+            
+            var val = $(this).val().toLowerCase();
+            var results = getSearchResults(val);
+            var s_stores = results.stores;
+            
+            if(s_stores != undefined && s_stores.length > 0){
+                $.each(s_stores, function(i, v){
+                    var div_stores = "<div class='store_search_list'>";
+                    div_stores = div_stores + "<h4><a href='/stores/" + v.slug + "'>" + v.name + "</a></h4>";
+                    div_stores = div_stores + "</div>";
+                    $('#store_search_results').append(div_stores);
+                    $('#store_search_results').show();
+                });
+            }
+        }
+    });
+}
