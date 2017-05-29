58570810a90fc5c82840d939bcf9a0a9c618dc39
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-29 09:54:33 -0400

    The Gateway 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,32 +1,12 @@
 function init(e) {
     
-    $(".dd_menu").click(function() {
-        var id = $(this).attr("id");
-        show_dd(id);
-    });
-
-    function show_dd(id){
-        if ($("#s_sub_"+ id ).is(":visible")){ 
-            $("#triangle_"+ id ).hide();
-            $(".submenu_bar").slideUp()
-            $("#s_sub_"+ id ).hide();               
-        } else {
-            if ($(".submenu_bar").is(":visible")){
-                $(".dd_menu").css({"background":"white", "color":"#4d4d4f"});
-                $(".triangle_down").hide();
-                $("#triangle_"+id).show();
-                $(".dd_submenu").hide();
-                $("#s_sub_"+id).show();
-                // $("#m"+id).css({"background":"#7b929d", "color":"white"});
-            } else {
-                $("#triangle_"+id).show();
-                $("#s_sub_"+id).show();
-                $(".submenu_bar").slideDown()
-                // $("#m"+id).css({"background":"#7b929d", "color":"white"});
-                
-            }
-            
-        }
-    }
     
+}
+
+function show_content(){
+    $('.yield').fadeIn();
+    $(".modal-backdrop").remove();
+    
+    var today_hours = getTodaysHours();
+    renderHomeHours('#home_hours_container', '#home_hours_template', today_hours)
 }
