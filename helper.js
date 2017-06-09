3557d0ab526ed77f545f3e4b8ef71264f888bfc8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-09 09:38:14 -0400

    The Gateway 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -63,6 +63,18 @@ function init(e) {
             $('.news-ticker').hide();
         }
     });
+    
+    //WEATHER
+    $.ajax({
+        url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            var location = parsed_json['location']['city'];
+            var temp_f = parsed_json['current_observation']['temp_f'];
+            alert("Current temperature in " + location + " is: " + temp_f);
+        }
+    });
+});
 }
 
 function render_instagram(data){
