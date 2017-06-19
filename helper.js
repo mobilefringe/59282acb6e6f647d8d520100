ed4a166cd7c1e0e8d5f3a26ee930ef6887564d7d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-19 13:46:12 -0400

    The Gateway 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -95,27 +95,27 @@ function show_content(){
                 $.each(messages, function(key ,val){
                     $('#bulletin').append('<li class="marquee caps"><strong>' + val.messages[0].title + ' </strong>'  + val.messages[0].body +  '</li>')
                 });
-                if ($('#bulletin li').length > 1){
-                    var tickerSpeed = $('.news-ticker').attr('data-speed');
-                    $('.news-ticker ul li').hide();
-                    $('.news-ticker ul li:first').show();
+                // if ($('#bulletin li').length > 1){
+                //     var tickerSpeed = $('.news-ticker').attr('data-speed');
+                //     $('.news-ticker ul li').hide();
+                //     $('.news-ticker ul li:first').show();
                     
-                    var currentSlide = 0;
-                    var slideCount = ($('.news-ticker li').length) - 1;
+                //     var currentSlide = 0;
+                //     var slideCount = ($('.news-ticker li').length) - 1;
                     
-                    var startTicker = setInterval(function(){
-                        $('.news-ticker ul li').eq(currentSlide).fadeOut(500)
+                //     var startTicker = setInterval(function(){
+                //         $('.news-ticker ul li').eq(currentSlide).fadeOut(500)
                         
-                        if (currentSlide < slideCount) {
-                            currentSlide += 1;
-                        } else {
-                            currentSlide = 0;
-                        }
+                //         if (currentSlide < slideCount) {
+                //             currentSlide += 1;
+                //         } else {
+                //             currentSlide = 0;
+                //         }
                         
-                        $('.news-ticker ul li').eq(currentSlide).fadeIn(500)
+                //         $('.news-ticker ul li').eq(currentSlide).fadeIn(500)
                     
-                    }, tickerSpeed);
-                }
+                //     }, tickerSpeed);
+                // }
             $('.news-ticker').show();
             }
         } else {
