cf10b77a7f107960ca52f7dd3d57bb42f63fcb3d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-07-13 16:34:22 -0400

    The Gateway 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -114,7 +114,7 @@ function render_instagram(data){
 }
 
 function get_instagram_feed(url, total, size, callback){
-    var html = '<div class="insta ih-item circle effect19"><a class="ig-image" target="_blank" href="{{{link}}}"><img src="{{{image}}}" alt="{{caption}}"/><div class="info"><h2>{{{caption}}}</h2></div></a>'
+    // var html = '<div class="insta ih-item circle effect19"><a class="ig-image" target="_blank" href="{{{link}}}"><img src="{{{image}}}" alt="{{caption}}"/><div class="info"><h2>{{{caption}}}</h2></div></a>'
     var item_rendered = [];
     Mustache.parse(html); 
     log('fetching instagram data from: ' + url);
@@ -124,6 +124,7 @@ function get_instagram_feed(url, total, size, callback){
             main_feed = insta_feed.splice(1,6);
             $.each(main_feed, function(i,v){
                 var feed_obj = {}
+                var html = '<div class="insta ih-item circle effect19"><a class="ig-image" target="_blank" href="{{{link}}}"><img src="{{{image}}}" alt="{{caption}}"/><div class="info"><h2>{{{caption}}}</h2></div></a>'
                 if(v.caption != null){
                     feed_obj.caption = v.caption.text
                 }
