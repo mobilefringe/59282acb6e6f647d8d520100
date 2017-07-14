486093f64ac912987a22330f974ae2369b27d11b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-07-14 15:46:52 -0400

    The Gateway 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -114,7 +114,7 @@ function render_instagram(data){
 }
 
 function get_instagram_feed(url, total, size, callback){
-    var html = '<div class="insta ih-item circle effect19"><a class="ig-image" target="_blank" href="{{{link}}}"><img src="{{{image}}}" alt="{{caption}}"/><div class="info"><div class="content"><h5>{{{caption_short}}}</h5></div></div></a></div>'
+    var html = '<div class="insta ih-item circle effect19"><a class="ig-image" target="_blank" href="{{{link}}}"><img src="{{{image}}}" alt="{{caption}}"/><div class="info"><div class="content"><p>{{{caption_short}}}</p></div></div></a></div>'
     var item_rendered = [];
     Mustache.parse(html); 
     log('fetching instagram data from: ' + url);
