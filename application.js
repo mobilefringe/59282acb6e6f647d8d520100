72ce740da56577fecf36b15fafd2099fda845dc2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-03 11:38:17 -0400

    The Gateway 1.0  /application.js

--- application.js
+++ application.js
@@ -394,7 +394,7 @@ function renderSinglePost(container, template, main_post){
     }
     main_post.description_short = main_post.description_short.replace("&amp;", "&");
     
-    main_post.slug = "blog/" + main_post.slug;
+    main_post.slug = "posts/" + main_post.slug;
     
     main_post.twitter_title = main_post.title + " via @ShopTheGateway";
     
