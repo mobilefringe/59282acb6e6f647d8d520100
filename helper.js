2b581c6e7ea15f1d513b0172e34c9e5600402a8f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-03 13:43:43 -0400

    The Gateway 1.2  /helper.js

--- helper.js
+++ helper.js
@@ -15,7 +15,7 @@ function init(e) {
     				top : '0'
     			});
     			$('.nav_container').addClass('sticky');
-    			$('.nav_container').slideDown();
+    			$('.nav_container').fadeIn();
     		}
     		if($(window).scrollTop() < scrollTop){
     			$('.nav_container').removeAttr('style');	
